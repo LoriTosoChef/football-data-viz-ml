23383efd65e6ffc696174cb78bfbbf991d5f846c
LoriTosoChef <lorenzo.tosoni.main@gmail.com>
2023-02-05 19:09:01 +0100

    improved preprocessing, generalized player_ratings data frame, ready for next dataset

--- scripts/preprocessing.R
+++ scripts/preprocessing.R
@@ -5,13 +5,13 @@ library("tidyverse")
 
 # Multi-Class Classification: Win H - Win A - Draw -----------------------------
 
+player_ratings <- player_stats %>%
+  group_by(player_api_id, date) %>%
+  summarise_if(is.numeric, mean)
+
 ## Overall Ratings Only --------------------------------------------------------
 # first attempt, without betting odds columns;
 # only using average overall rating of each team
-
-overall_rating_players <- player_stats %>%
-  group_by(player_api_id, date) %>%
-  summarise(overall_rating = mean(overall_rating))
 
 # adding target column (Y)
 # 0: Home Team Win --- 1: Away Team Win --- 2: Draw
@@ -19,27 +19,27 @@ overall_rating_players <- player_stats %>%
 dataset_player_overall <- match[1:33] %>%
   mutate(target = ifelse(home_team_goal > away_team_goal, "H", ifelse(away_team_goal > home_team_goal, "A", "D"))) %>%
   select(-id, -country_id, -league_id, -season, -stage, -date, -home_team_goal, -away_team_goal) %>%
-  left_join(overall_rating_players, 
+  left_join(player_ratings %>% select(player_api_id, date, overall_rating), 
              by = c('home_player_1' = 'player_api_id', 'season_year' = 'date')) %>%
-  left_join(overall_rating_players, 
+  left_join(player_ratings %>% select(player_api_id, date, overall_rating),
              by = c('home_player_2' = 'player_api_id', 'season_year' = 'date'), suffix = c('_h1', '_h2')) %>%
-  left_join(overall_rating_players,
+  left_join(player_ratings %>% select(player_api_id, date, overall_rating),
             by = c('home_player_3' = 'player_api_id', 'season_year' = 'date')) %>%
-  left_join(overall_rating_players,
+  left_join(player_ratings %>% select(player_api_id, date, overall_rating),
             by = c('home_player_4' = 'player_api_id', 'season_year' = 'date'), suffix = c('_h3', '_h4')) %>%
-  left_join(overall_rating_players, 
+  left_join(player_ratings %>% select(player_api_id, date, overall_rating), 
             by = c('home_player_5' = 'player_api_id', 'season_year' = 'date')) %>%
-  left_join(overall_rating_players, 
+  left_join(player_ratings %>% select(player_api_id, date, overall_rating), 
             by = c('home_player_6' = 'player_api_id', 'season_year' = 'date'), suffix = c('_h5', '_h6')) %>%
-  left_join(overall_rating_players,
+  left_join(player_ratings %>% select(player_api_id, date, overall_rating),
             by = c('home_player_7' = 'player_api_id', 'season_year' = 'date')) %>%
-  left_join(overall_rating_players,
+  left_join(player_ratings %>% select(player_api_id, date, overall_rating),
             by = c('home_player_8' = 'player_api_id', 'season_year' = 'date'), suffix = c('_h7', '_h8')) %>%
-  left_join(overall_rating_players, 
+  left_join(player_ratings %>% select(player_api_id, date, overall_rating),
             by = c('home_player_9' = 'player_api_id', 'season_year' = 'date')) %>%
-  left_join(overall_rating_players, 
+  left_join(player_ratings %>% select(player_api_id, date, overall_rating),
             by = c('home_player_10' = 'player_api_id', 'season_year' = 'date'), suffix = c('_h9', '_h10')) %>%
-  left_join(overall_rating_players,
+  left_join(player_ratings %>% select(player_api_id, date, overall_rating),
             by = c('home_player_11' = 'player_api_id', 'season_year' = 'date')) %>%
   select(-home_player_1, -home_player_2, -home_player_3, -home_player_4, -home_player_5, -home_player_6, 
          -home_player_7, -home_player_8, -home_player_9, -home_player_10, -home_player_11)
@@ -53,27 +53,27 @@ dataset_player_overall <- dataset_player_overall %>%
 
 # left joining away team players
 dataset_player_overall <- dataset_player_overall %>%
-  left_join(overall_rating_players, 
+  left_join(player_ratings %>% select(player_api_id, date, overall_rating), 
             by = c('away_player_1' = 'player_api_id', 'season_year' = 'date')) %>%
-  left_join(overall_rating_players, 
+  left_join(player_ratings %>% select(player_api_id, date, overall_rating), 
             by = c('away_player_2' = 'player_api_id', 'season_year' = 'date'), suffix = c('_a1', '_a2')) %>%
-  left_join(overall_rating_players,
+  left_join(player_ratings %>% select(player_api_id, date, overall_rating),
             by = c('away_player_3' = 'player_api_id', 'season_year' = 'date')) %>%
-  left_join(overall_rating_players,
+  left_join(player_ratings %>% select(player_api_id, date, overall_rating),
             by = c('away_player_4' = 'player_api_id', 'season_year' = 'date'), suffix = c('_a3', '_a4')) %>%
-  left_join(overall_rating_players, 
+  left_join(player_ratings %>% select(player_api_id, date, overall_rating),
             by = c('away_player_5' = 'player_api_id', 'season_year' = 'date')) %>%
-  left_join(overall_rating_players, 
+  left_join(player_ratings %>% select(player_api_id, date, overall_rating),
             by = c('away_player_6' = 'player_api_id', 'season_year' = 'date'), suffix = c('_a5', '_a6')) %>%
-  left_join(overall_rating_players,
+  left_join(player_ratings %>% select(player_api_id, date, overall_rating),
             by = c('away_player_7' = 'player_api_id', 'season_year' = 'date')) %>%
-  left_join(overall_rating_players,
+  left_join(player_ratings %>% select(player_api_id, date, overall_rating),
             by = c('away_player_8' = 'player_api_id', 'season_year' = 'date'), suffix = c('_a7', '_a8')) %>%
-  left_join(overall_rating_players, 
+  left_join(player_ratings %>% select(player_api_id, date, overall_rating),
             by = c('away_player_9' = 'player_api_id', 'season_year' = 'date')) %>%
-  left_join(overall_rating_players, 
+  left_join(player_ratings %>% select(player_api_id, date, overall_rating),
             by = c('away_player_10' = 'player_api_id', 'season_year' = 'date'), suffix = c('_a9', '_a10')) %>%
-  left_join(overall_rating_players,
+  left_join(player_ratings %>% select(player_api_id, date, overall_rating),
             by = c('away_player_11' = 'player_api_id', 'season_year' = 'date')) %>%
   select(-away_player_1, -away_player_2, -away_player_3, -away_player_4, -away_player_5, -away_player_6, 
          -away_player_7, -away_player_8, -away_player_9, -away_player_10, -away_player_11)
